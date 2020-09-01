6127b3eb09aa1b8a39d74492a699b2849e1366a9
Marina Mastropierro <marinamastropierro8@gmail.com>
2020-09-01 16:14:27 -0300

    agrego bootstrap

--- client/src/component/FormCRUD.js
+++ client/src/component/FormCRUD.js
@@ -5,23 +5,23 @@ return (
     <div>
         <form>
             <legend>Crear Producto</legend>
-            <div>
+            <div class="form-group">
                 <label for="name">Nombre</label> 
                 <input type="text" id="name" name="name" />
             </div>
-            <div>
+            <div class="form-group">
                 <label for="descripcion">Descripción</label> 
                 <input type="text" id="descripcion" name="descripcion"/>
             </div>
-            <div>
+            <div class="form-group">
                 <label for="categoria">Categoría</label> 
                 <input type="text" id="categoria" name="categoria"/>
             </div>
-            <div>
+            <div class="form-group">
                <label for="precio">Precio</label> 
                 <input type="number" id="precio" name="precio" />
             </div>
-            <div>
+            <div class="form-group">
                <label for="imagen">Imagen</label> 
                 <input type="text" id="imagen" name="imagen"/>
             </div>
@@ -29,32 +29,35 @@ return (
         </form>
         <form>
             <legend>Editar Producto</legend>
-            <div>
+            <div class="form-group">
                 <label for="name">Nombre</label> 
                 <input type="text" id="name" name="name" />
             </div>
-            <div>
+            <div class="form-group">
                 <label for="descripcion">Descripción</label> 
                 <input type="text" id="descripcion" name="descripcion"/>
             </div>
-            <div>
+            <div class="form-group">
                 <label for="categoria">Categoría</label> 
-                <input type="text" id="categoria" name="categoria"/>
+                <input type="text" id="categoria" name="categoria" />
             </div>
-            <div>
+            <div class="form-group">
                <label for="precio">Precio</label> 
-                <input type="number" id="precio" name="precio" />
+                <input type="number" id="precio" name="precio"/>
             </div>
-            <div>
+            <div class="form-group">
                <label for="imagen">Imagen</label> 
-                <input type="text" id="imagen" name="imagen"/>
+                <input type="text" id="imagen" name="imagen" />
             </div>
                 <input type="submit" value="Editar"/>
         </form>
         <form>
             <legend>Eliminar Producto</legend>
+                <div className="form-group">
                 <label for="idProduct">Id Producto</label> 
                 <input type="text" id="idProduct" name="idProduct" />
+                </div>
+               
                 <input type="submit" value="Eliminar"/>
         </form>
     </div>
