3b139589308ff9cd9654a2d40a1f72096e140950
Marina Mastropierro <marinamastropierro8@gmail.com>
2020-09-01 15:46:13 -0300

    mejora

--- client/src/component/FormCRUD.js
+++ client/src/component/FormCRUD.js
@@ -5,37 +5,57 @@ return (
     <div>
         <form>
             <legend>Crear Producto</legend>
+            <div>
                 <label for="name">Nombre</label> 
                 <input type="text" id="name" name="name" />
+            </div>
+            <div>
                 <label for="descripcion">Descripción</label> 
                 <input type="text" id="descripcion" name="descripcion"/>
+            </div>
+            <div>
                 <label for="categoria">Categoría</label> 
                 <input type="text" id="categoria" name="categoria"/>
-                <label for="precio">Precio</label> 
+            </div>
+            <div>
+               <label for="precio">Precio</label> 
                 <input type="number" id="precio" name="precio" />
-                <label for="imagen">Imagen</label> 
+            </div>
+            <div>
+               <label for="imagen">Imagen</label> 
                 <input type="text" id="imagen" name="imagen"/>
-                <input type="submit">Crear Producto</input>
+            </div>
+            <input type="submit" value="Crear"/>
         </form>
         <form>
             <legend>Editar Producto</legend>
+            <div>
                 <label for="name">Nombre</label> 
                 <input type="text" id="name" name="name" />
+            </div>
+            <div>
                 <label for="descripcion">Descripción</label> 
                 <input type="text" id="descripcion" name="descripcion"/>
+            </div>
+            <div>
                 <label for="categoria">Categoría</label> 
                 <input type="text" id="categoria" name="categoria"/>
-                <label for="precio">Precio</label> 
+            </div>
+            <div>
+               <label for="precio">Precio</label> 
                 <input type="number" id="precio" name="precio" />
-                <label for="imagen">Imagen</label> 
+            </div>
+            <div>
+               <label for="imagen">Imagen</label> 
                 <input type="text" id="imagen" name="imagen"/>
-                <input type="submit">Editar Producto</input>
+            </div>
+                <input type="submit" value="Editar"/>
         </form>
         <form>
             <legend>Eliminar Producto</legend>
                 <label for="idProduct">Id Producto</label> 
                 <input type="text" id="idProduct" name="idProduct" />
-                <input type="submit">Eliminar Producto</input>
+                <input type="submit" value="Eliminar"/>
         </form>
     </div>
     
