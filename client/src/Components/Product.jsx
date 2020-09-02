25f8c06e4571a57ec9e0750ec08f292d8e0cbf7d
Victor Guiliana <victorguiliana@gmail.com>
2020-09-02 12:38:24 -0300

    Modico el componente Producto

--- client/src/Components/Product.jsx
+++ client/src/Components/Product.jsx
@@ -2,14 +2,29 @@ import { React } from 'react';
 import { Route, Link } from 'react-router-dom';
 
 
-const Product = ({ titulo, descripcion, precio, cantidad }) => {
+const Product = ({ name, description, price, stock, image, size }) => {
     return (
-        <div>
-            <span>{titulo}</span>
-            <span>{descripcion}</span>
-            <span>{precio}</span>
-            <span>{cantidad}</span>
+        <div class="card" style="width: 18rem;">
+
+            <img src={image} class="card-img-top" alt="Product Image"/>
+
+            <div class="card-body">
+                <h5 class="card-title">{name}</h5>
+                <p class="card-text">{description}</p>
+            </div>
+
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Size: {size}</li>
+                <li class="list-group-item">Precio: {price}</li>
+                <li class="list-group-item">Stock: {stock}</li>
+            </ul>
+            
+            <div class="card-body">
+                <a href="#" class="card-link">Agregar al carrito</a>
+            </div>
+
         </div>
     )
+};
 
-}
+export default Product;
