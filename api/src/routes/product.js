38bc064931124a5b1cb66f9e51717b56c2404d7f
tomp <you@example.com>
2020-09-02 18:42:12 -0300

    Añadi una llave y un parentesis

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,40 +1,36 @@
 const server = require('express').Router();
-
-
-
 const { Product,Category,ProductCategory } = require('../db.js');
-
 
 server.get('/', (req, res, next) => {
 	Product.findAll()
 		.then(products => {
 			res.send(products);
-		})
-		.catch(next);
+		}).catch(next);
 });
 
 
-server.post("/:idProducto/category/:idCategoria",(req,res)=>{
+server.post("/:idProducto/category/:idCategoria",(req,res) => {
 	const id = req.params.idProducto;
 	const idCategoria = req.params.idCategoria;
 	ProductCategory.create({ProductId:id,CategoryId:idCategoria})
-	.then(pc=>res.send(pc))
-	.catch(err=>res.send(err))
+	.then(pc => res.send(pc))
+	.catch(err => res.send(err))
 })
 
-server.delete("/:idProducto/category/:idCategoria",(req,res)=>{
+server.delete("/:idProducto/category/:idCategoria",(req,res) => {
 	ProductCategory.destroy({where:{ProductId:req.params.id,CategoryId:req.params.idCategoria}})
 	.then(deletedRecord => {
 		if (deletedRecord === 1) { res.status(200).json({ message: "Review cleared successfully" }); }
-		else { res.status(404).json({ message: "Review not found" })}
+		else { res.status(404).json({ message: "Review not found" }); }
+	})
 })
 
-server.post("/category",(req,res)=>{
+server.post("/category",(req,res) => {
 	//suponemos que la data viene por body
 Category.create({name:req.body.name,description:req.body.description})
 .then(category=>res.send(category))
 .catch(err=>res.send(err))
-
 })
+
 
 module.exports = server;
