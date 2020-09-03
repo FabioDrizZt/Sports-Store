4e1fdfb3d0645ff7347a6701f65e17ff30a53ebd
arieltecay <arieltecay@gmail.com>
2020-09-03 12:39:45 -0300

    Creacion de ProductCard

---
+++ client/src/Components/Catalogo.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ProductCard from './ProductCard'
+
+function Catalogo (props){
+    return (
+    <div>
+        <ProductCard 
+            
+        />
+    </div>)
+}
+
+export default Catalogo;
+//El Catalogo muestra una grilla de Componentes ProductCard.
+//Recibe por props un arreglo de productos.
