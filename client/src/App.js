33b594ca47f49e904f8d64b1c322a596bda45111
Gabriel Apaza <gabriel.oaq1@gmail.com>
2020-09-03 12:29:43 -0300

    S14 y S15, creamos Products.jsx

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,45 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { useSelector } from "react-redux";
 //import logo from './logo.svg';
 import './App.css';
-import Product from './Components/Product.jsx'
-import SearchBar from './Components/SearchBar.jsx'
+import Product from './Components/Product.jsx';
+import SearchBar from './Components/SearchBar.jsx';
+import Products from './Components/Products.jsx';
+
+
 
 function App() {
+  const products= useSelector(state => state.products)
+
+  function onFilterId(Id) {
+    var productId = products.filter(products=> products.id == Id)
+    if (productId !== undefined) {
+      return productId [0];
+    } else {
+      return null;
+    }
+  }
   return (
     <div className="App">
       <Route
-        exact path='/Product'
+        exact path='/product'
         render={() => <Product />}
       />
       <Route
-        exact path='/SearchBar'
+        exact path='/searchBar'
         render={() => <SearchBar />}
       />
+      {/* S14 : Crear Ruta para mostrar el componente Catalogo */}
+      <Route
+        exact path='/products'
+        render={() => <Products />}
+      />
+      {/* S15 : Crear Ruta para ver el detalle de un producto según el id. */}
+      <Route
+          exact path='/products/:id'
+          render={({ match }) => <Product productName={onFilterId(match.params.id)} />}
+        />
     </div>
   );
 }
