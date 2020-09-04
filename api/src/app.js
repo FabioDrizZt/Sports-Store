7ba27368268c9213cb535a39928544c4211310c3
Marina Mastropierro <marinamastropierro8@gmail.com>
2020-09-04 15:56:09 -0300

    ruta eliminar producto,funcionalidad boton eliminar,componenete formcrud

--- api/src/app.js
+++ api/src/app.js
@@ -4,9 +4,12 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
 
+
 require('./db.js');
 
 const server = express();
+var cors = require ('cors');
+server.use (cors ());
 
 server.name = 'API';
 
@@ -18,6 +21,7 @@ server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
 
