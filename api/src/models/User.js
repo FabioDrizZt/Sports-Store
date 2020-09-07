65ea34496a7ae1a1d80db8cba227e4a8f7de7c68
tomp <you@example.com>
2020-09-07 16:39:11 -0300

    Modelo User - Relacionado

---
+++ api/src/models/User.js
@@ -0,0 +1,35 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  sequelize.define('user', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lastName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+    DNI: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate:{
+            isEmail : true
+        }
+      },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+    role: {
+        type: DataTypes.ENUM({
+            values: ['admin', 'user']
+        })
+      },
+  })
+};
