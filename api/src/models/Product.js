a0bb201ad3d83cf590ed40558bc65c808c9eb4d4
unknown <drizzzt@gmail.com>
2020-09-07 14:51:12 -0300

    se estaba guardando mal la descripción del prod

--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -7,13 +7,13 @@ module.exports = (sequelize) => {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
-      set(value) {this.setDataValue('name', value.toLowerCase())}
+      set(value) { this.setDataValue('name', value.toLowerCase()) }
     }, size: {
       type: DataTypes.STRING,
     }, description: {
       type: DataTypes.TEXT,
       defaultValue: "Este producto no tiene descripción.",
-      set(value) {this.setDataValue('name', value.toLowerCase())}
+      set(value) { this.setDataValue('description', value.toLowerCase()) }
     }, price: {
       type: DataTypes.FLOAT,
       allowNull: false,
