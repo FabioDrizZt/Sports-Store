93c9760b169607317b814efe5dfe7e8527286a44
unknown <drizzzt@gmail.com>
2020-09-07 17:02:28 -0300

    searchbar

--- client/src/Components/SearchBar.jsx
+++ client/src/Components/SearchBar.jsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { searchProducts } from "../actions";
 import { Link } from "react-router-dom";
 import "./SearchBar.css";
+
 export default function SearchBar() {
+  const [input, setInput] = useState("");
+  const dispatch = useDispatch();
+
   return (
     <form className="form-inline my-2 my-lg-0">
-      <input className="search form-control mr-sm-2" type="search" placeholder="Articulo..." />
-      <Link to="/Product">
-      <button class="btn btn-success my-2 my-sm-0" type="submit">BUSCAR</button>
+      <input
+        className="search form-control mr-sm-2"
+        type="search"
+        placeholder="Articulo..."
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <Link to="/Products">
+        <button
+          class="btn btn-success my-2 my-sm-0"
+          type="submit"
+          onClick={(e) => dispatch(searchProducts(input))}
+        >
+          BUSCAR
+        </button>
       </Link>
     </form>
   );
 }
-
-// onclick={()=>searchProduct(input)}
