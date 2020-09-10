208a3fd59cbebd9603b5de6a19e48d98d93fc58e
tomp <you@example.com>
2020-09-10 00:45:52 -0300

    CSS a 1000! Incio-catalogo

--- client/src/Components/ProductCard.jsx
+++ client/src/Components/ProductCard.jsx
@@ -9,23 +9,22 @@ function ProductCard({ id, titulo, descripcion, precio, cantidad, imagen }) {
         <div className="card col-sm-4 cardStyle">
         
             <div className="card-body text-left">
-            <img className="card-img-top img" src={imagen} alt="Imagen Producto" />
-            <div className="card-title">    
-                <label>
+            <div className="containerImgTitle titleCard card-title">
                 <Link to={`/products/${id}`}>
+                    <img className="card-img-top img" src={imagen} alt="Imagen Producto" />
                     <h3 className="card-title">{ titulo }</h3>     
                 </Link>
-                </label>
             </div>
+            
                 <hr/>
 
                 <h5 className="card-text">Descripcion del producto</h5>
                 <p>{ descripcion }</p>
                 <p>{ cantidad }</p>
-            <div className="button">    
+            <div className="button">
                 <p className="price"><b>$ { precio }</b></p>
                 <button className="btn btn-warning ">Ver mas</button>
-                <button className="btn btn-success ">Agregar a Carrito</button>
+                <button className="btn b ">Agregar a Carrito</button>
             </div>
             </div>
         </div>
