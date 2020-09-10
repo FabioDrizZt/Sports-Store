b86109a1a6437e7170022c2ff4a16b9ae160bd68
Marina Mastropierro <marinamastropierro8@gmail.com>
2020-09-10 20:54:18 -0300

    carrito

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import SearchBar from "../SearchBar.jsx";
 import {Link} from "react-router-dom";
 import "./NavBar.css";
+import cart from "../../cart.png"
 
 export default function NavBar() {
 
@@ -22,6 +23,9 @@ export default function NavBar() {
           <div>
           <SearchBar />
           </div>
+          <Link to="/cart">
+          <img src={cart} alt="cart" className="mr-4"/>
+          </Link>
         </header>
         <aside className="font sidebar">
           <button className="sidebar-close-button" onClick={closeMenu}>
