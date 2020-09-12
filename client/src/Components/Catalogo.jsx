48bea513a42655f7018984313b3757ad544f533a
tomp <you@example.com>
2020-09-12 15:32:34 -0300

    Parte 2

--- client/src/Components/Catalogo.jsx
+++ client/src/Components/Catalogo.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import ProductCard from "./ProductCard";
 import { useSelector, useDispatch } from "react-redux";
 import { getProducts, getCategories, getCategoryProducts } from "../actions";
