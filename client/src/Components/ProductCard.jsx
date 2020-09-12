efba14a07b30f0aa28de87e9f3f62129ba731d21
tomp <you@example.com>
2020-09-12 00:15:23 -0300

    Arreglos varios css

--- client/src/Components/ProductCard.jsx
+++ client/src/Components/ProductCard.jsx
@@ -5,7 +5,7 @@ import {addtoCart} from "../actions/index";
 import { useSelector,useDispatch } from "react-redux";
 
 // Estaria bueno que cada producto tenga un subtitulo
-function ProductCard({ id, titulo, descripcion, precio, cantidad, imagen,stock }) {
+function ProductCard({ id, titulo, descripcion, precio, cantidad, imagen,stock,categories }) {
     const user = useSelector(state=>state.user);
     const dispatch = useDispatch();
     //NO HAY NADA EN EL STORE USER TODAVIA
@@ -14,34 +14,28 @@ function ProductCard({ id, titulo, descripcion, precio, cantidad, imagen,stock }
       }
     
     return (
-        <div className=" col-sm-4 cardStyle p-4">
-        
-            <div className="text-left">
-            
+        <div className=" col-sm-4 cardStyle p-4">        
+            <div className="text-left">            
                 <Link to={`/products/${id}`}>
                     <img className="card-img-top img" src={imagen} alt="Imagen Producto" />
                     <h3 className="card-title">{ titulo }</h3>     
-                </Link>
-                
+                </Link>    
+               {categories&&categories.map(x=><span class="badge badge-secondary mr-1">{x.name}</span>)}           
                 <hr/>
-
                 <h5 className="card-text">Descripcion del producto</h5>
                 <p>{ descripcion }</p>
-                <p>{ cantidad }</p>
-           
+                <p>{ cantidad }</p>           
                 <p className="price"><b>$ { precio }</b></p>
                 <Link to={`/products/${id}`}>
-                <button className="b btn btn-warning">Ver mas</button>
+                <button className="b btn">Ver mas</button>
                 </Link>
                 <Link to="/cart">
                 <button 
                 onClick={()=>agregarAlCarrito(id,precio,cantidad)}
-                className={stock===0?"b btn btn-secondary":"btn btn-success"} 
+                className={stock===0?"btn btn-secondary":"btn btn-success"} 
                 disabled={stock===0?true:false}>
                     {stock===0 ? "No disponible": "Agregar a Carrito"}</button>
                     </Link>
-            
-           
             </div>
         </div>
     )
