e263b0950c3d83b2fb16a9aaa7d422fe76ffc89d
unknown <drizzzt@gmail.com>
2020-09-14 13:41:34 -0300

    Agregado el iconito del usuario al componente NavBar

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -1,62 +1,73 @@
 import React, { useEffect } from "react";
 import SearchBar from "../SearchBar.jsx";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./NavBar.css";
-import cart from "../../cart.png"
+import cart from "../../cart.png";
+import login from "../../login.png";
 
 export default function NavBar() {
-
   useEffect(() => {
-    document.body.addEventListener('click', closeMenu );
-},[]);
+    document.body.addEventListener("click", closeMenu);
+  }, []);
 
   const openMenu = () => {
-    document.querySelector('.sidebar').classList.add('open');
+    document.querySelector(".sidebar").classList.add("open");
   };
   const closeMenu = () => {
-    document.querySelector('.sidebar').classList.remove('open');
+    document.querySelector(".sidebar").classList.remove("open");
   };
 
   return (
     <div className="grid-container">
-        <header className="header">
-          <div className="brand">
-            <button onClick={openMenu}>&#9776;</button>
-            <Link to="/">Sports Store</Link>
-          </div>
-          <div>
+      <header className="header">
+        <div className="brand">
+          <button onClick={openMenu}>&#9776;</button>
+          <Link to="/">Sports Store</Link>
+        </div>
+        <div>
           <SearchBar />
-          </div>
-          <Link to="/cart">
-          <img src={cart} alt="cart" className="mr-4 w-50"/>
-          </Link>
-        </header>
-        <aside className="font sidebar">
-          <button className="sidebar-close-button" onClick={closeMenu}>
-            X
-          </button>
-          <ul className="categories">
-            <li>
-              <Link className="option" to="/" onClick={closeMenu}>Inicio</Link>
-            </li>
-            <li>
-              <Link className="option" to="/products" onClick={closeMenu}>Productos</Link>
-            </li>
+        </div>
+        <Link to="/cart">
+          <img src={cart} alt="cart" className="mr-4 w-50" />
+        </Link>
+        <Link to="/login">
+          <img src={login} alt="login" className="mr-4 w-50" />
+        </Link>
+      </header>
+      <aside className="font sidebar">
+        <button className="sidebar-close-button" onClick={closeMenu}>
+          X
+        </button>
+        <ul className="categories">
+          <li>
+            <Link className="option" to="/" onClick={closeMenu}>
+              Inicio
+            </Link>
+          </li>
+          <li>
+            <Link className="option" to="/products" onClick={closeMenu}>
+              Productos
+            </Link>
+          </li>
 
-            {/* <li>    -------> Estas opciones son solo para el Admin, no deberian estar en la nav
+          {/* <li>    -------> Estas opciones son solo para el Admin, no deberian estar en la nav
               <Link className="option" to="/formCrud">Productos</Link >
             </li> */}
-            {/* <li>
+          {/* <li>
               <Link className="option" to="/category">Categorías</Link>
             </li> */}
-            <li>
-              <Link className="option" to="/admin" onClick={closeMenu}>Admin</Link>
-            </li>           
-            <li>
-              <Link className="option" to="/users" onClick={closeMenu}>Usuarios</Link>
-            </li>
-          </ul>
-        </aside>
-      </div>
+          <li>
+            <Link className="option" to="/admin" onClick={closeMenu}>
+              Admin
+            </Link>
+          </li>
+          <li>
+            <Link className="option" to="/users" onClick={closeMenu}>
+              Usuarios
+            </Link>
+          </li>
+        </ul>
+      </aside>
+    </div>
   );
 }
