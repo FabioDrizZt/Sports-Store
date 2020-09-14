2cc580cdb46446a7c37dced0ab7997fb5103bbd2
arieltecay <arieltecay@gmail.com>
2020-09-14 00:01:01 -0300

    Validando campos de Email y DNI para no repetirse

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -12,7 +12,8 @@ module.exports = (sequelize) => {
       },
     DNI: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+        unique:true
       },
     email: {
         type: DataTypes.STRING,
