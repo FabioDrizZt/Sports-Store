a2459fdb7cc0fb799c043ec8303dd37f5cbf4e8e
unknown <drizzzt@gmail.com>
2020-09-13 21:33:50 -0300

    app refactor e inclusión de prettier/eslint

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,9 @@
 import React from "react";
 import { Route } from "react-router-dom";
-// import { useSelector } from "react-redux";
-//import logo from './logo.svg';
 import "./App.css";
 import Catalogo from "./Components/Catalogo";
 import Product from "./Components/Product.jsx";
-// import CatalogFilter from './Components/CatalogFilter.jsx';
 import NavBar from "./Components/NavBar/NavBar.jsx";
-import "./Components/NavBar/NavBar.css";
 import EditProduct from "./Components/FormCRUD/EditProduct.jsx";
 import FormCrud from "./Components/FormCRUD/FormCrud";
 import UserCrud from "./Components/FormCRUD/UserCrud";
@@ -20,64 +16,40 @@ import OrdersTable from "./Components/OrderTable/OrdersTable";
 import CreateProduct from './Components/FormCRUD/CreateProduct';
 
 function App() {
-  // const products= useSelector(state => state.products)
-
-  // function onFilterId(Id) {
-  //   var productId = products.filter(products=> products.id == Id)
-  //   if (productId !== undefined) {
-  //     return productId [0];
-  //   } else {
-  //     return null;
-  //   }
-  // }
-
-  // Scroll up (cuando le damos click al boton)
-
-  document.getElementById("button-up").addEventListener("click", scrollUp);
+  const scroll = document.documentElement.scrollTop;
+  const buttonUp = document.getElementById("button-up");
+  buttonUp.addEventListener("click", scrollUp);
 
   function scrollUp() {
-    var currentScroll = document.documentElement.scrollTop;
-    if (currentScroll > 0) {
+    if (scroll > 0) {
       window.requestAnimationFrame(scrollUp);
-      window.scrollTo(0, currentScroll - currentScroll / 10);
+      window.scrollTo(0, scroll - scroll / 10);
       buttonUp.style.transform = "scale(0)";
     }
   }
 
-  // ---------------------------------------------
-  //scroll para que aparezca el boton
-
-  var buttonUp = document.getElementById("button-up");
-
-  window.onscroll = function () {
-    var scroll = document.documentElement.scrollTop;
-
-    if (scroll > 500) {
-      buttonUp.style.transform = "scale(1)";
-    } else if (scroll < 500) {
-      buttonUp.style.transform = "scale(0)";
-    }
+  window.onscroll = () => {
+    (document.documentElement.scrollTop > 500) ? buttonUp.style.transform = "scale(1)" : buttonUp.style.transform = "scale(0)";
   };
-  // -----------------------------------------------
 
   return (
     <div className="App">
       <NavBar />
-      <Route exact path="/products/:id" render={(match) => <Product match={match} />} />
       <Route exact path="/" render={() => <Home />} />
-      <Route exact path="/products" render={() => <Catalogo />} />
+      <Route exact path="/cart" render={() => <Cart />} />
       <Route exact path="/category" render={() => <FormCategory />} />
       <Route exact path="/formcrud" component={FormCrud} />
+      <Route exact path='/order' render={() => <Order />} />
+      <Route exact path="/products/:id" render={(match) => <Product match={match} />} />
+      <Route exact path="/products" render={() => <Catalogo />} />
       <Route exact path="/users" component={UserCrud} />
-      <Route exact path="/cart" render={() => <Cart />} />    
-      <Route exact path='/order' render={() => <Order  />} />
-                {/* RUTAS DEL ADMINISTRADOR */}
-      <Route exact path="/admin/orders" render={() => <OrdersTable />} />
-      <Route exact path="/admin/edit/product/:id" render={(match) => <EditProduct match={match} />} />  
+      {/* RUTAS DEL ADMINISTRADOR */}
       <Route exact path='/admin' render={() => <Admin />} />
-      <Route exact path='/admin/categories' component={FormCategory}/>
-      <Route exact path='/admin/newProduct' component={CreateProduct}/>
-      <Route exact path='/admin/myProducts' component={FormCrud}/>
+      <Route exact path='/admin/categories' component={FormCategory} />
+      <Route exact path="/admin/edit/product/:id" render={(match) => <EditProduct match={match} />} />
+      <Route exact path='/admin/myProducts' component={FormCrud} />
+      <Route exact path='/admin/newProduct' component={CreateProduct} />
+      <Route exact path="/admin/orders" render={() => <OrdersTable />} />
     </div>
   );
 }
