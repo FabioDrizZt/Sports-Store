deca8f787b1e64449a603be6008229701eabf194
unknown <drizzzt@gmail.com>
2020-09-14 03:29:21 -0300

    agregados los dispatch de remover y updatearamounts

--- client/src/Components/Order.jsx
+++ client/src/Components/Order.jsx
@@ -1,38 +1,31 @@
 import React, { useState } from "react";
 import "./Order.css";
 import { useSelector, useDispatch } from "react-redux";
-//import { removeFromCart } from "../actions"
+import { removeOrder, updateOrderAmount } from "../actions";
 
-const Order = ({
-  id = 888888,
-  name = "88888",
-  description = "888888",
-  size = "888888",
-  price = 88888,  
-  stock = 88888,
-  image = "https://static.mercadoshops.com/zapatilla-salomon-speedcross-hombre-trail-running-v_iZ878255024XsZ230467303XpZ1XfZ230467303-23380324018-5XvZdxIM.jpg",
-}) => {
+const Order = ({order}) => {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user);
-  const [cantidad, setCantidad] = useState(1);
+  const [cantidad, setCantidad] = useState(order.amount);  
 
   function minusClick(e) {
     e.preventDefault();
-    cantidad !== 1
-      ? setCantidad(cantidad - 1)
-      : alert("no podes llevar menos de 1");
+    if (cantidad !== 1) {
+      setCantidad(cantidad - 1);
+      dispatch(updateOrderAmount(1, order));
+    } else alert("no podes llevar menos de 1");
   }
 
   function plusClick(e) {
     e.preventDefault();
-    cantidad !== stock
-      ? setCantidad(cantidad + 1)
-      : alert("no podes llevar mas de: " + stock);
+    if (cantidad !== order.product.stock) {
+      setCantidad(cantidad + 1);
+      dispatch(updateOrderAmount(1, order));
+    } else alert("no podes llevar mas de: " + order.product.stock);
   }
 
-  function deleteItem(user, producto) {
-    //   dispatch(removeFromCart(user, prod));
-    alert("Producto borra2");
+  function deleteItem() {
+    dispatch(removeOrder(order.id));
   }
 
   return (
@@ -40,54 +33,50 @@ const Order = ({
       <div className="media align-items-lg-center flex-column flex-lg-row p-3">
         <div className="media-body order-2 order-lg-1">
           <h2 className="mt-0 font-weight-bold mb-2">
-            {name} id:{id}
+            {order.product.name} ProdId:{order.product.id} OrderId {order.id}
           </h2>
-          <p className="font-italic text-muted mb-0 large">{description}</p>
+          <p className="font-italic text-muted mb-0 large">{order.product.description}</p>
           <div className="d-flex align-items-center justify-content-between mt-1">
-            <h3 className="font-weight-bold my-2">Precio: ${price}</h3>
+            <h3 className="font-weight-bold my-2">Precio: ${order.product.price}</h3>
 
-            <h5 className="font-weight-bold my-2">Stock: {stock}</h5>
-            <h5 className="font-weight-bold my-2">Talle: {size}</h5>
+            <h5 className="font-weight-bold my-2">Stock: {order.product.stock}</h5>
+            <h5 className="font-weight-bold my-2">Talle: {order.product.size}</h5>
             <span style={{ position: "-webkit-sticky", left: 0 }}>
-        <h3 className="font-weight-bold my-2">
-          <button
-            onClick={(e) => {
-              minusClick(e);
-            }}
-            style={{ width: "30px" }}
-            className="btn btn-outline-success btn-sm"
-          >
-            -
-          </button>
-          {"Cantidad:" + cantidad}
-          <button
-            onClick={(e) => {
-              plusClick(e);
-            }}
-            style={{ width: "30px" }}
-            className="btn btn-outline-success btn-sm "
-          >
-            +
-          </button>
-          SubTotal: ${cantidad * price}
-          <button
-            className="btn btn-danger"
-            onClick={() => deleteItem(user.id, id)}
-          >
-            Eliminar
-          </button>
-        </h3>
-      </span>
+              <h3 className="font-weight-bold my-2">
+                <button
+                  onClick={(e) => {
+                    minusClick(e);
+                  }}
+                  style={{ width: "30px" }}
+                  className="btn btn-outline-success btn-sm"
+                >
+                  -
+                </button>
+                {"Cantidad:" + cantidad}
+                <button
+                  onClick={(e) => {
+                    plusClick(e);
+                  }}
+                  style={{ width: "30px" }}
+                  className="btn btn-outline-success btn-sm "
+                >
+                  +
+                </button>
+                SubTotal: ${cantidad * order.product.price}
+                <button className="btn btn-danger" onClick={() => deleteItem()}>
+                  Eliminar
+                </button>
+              </h3>
+            </span>
           </div>
         </div>
         <img
-          src={image}
-          alt={name}
+          src={order.product.image}
+          alt={order.product.name}
           width="200"
           class="ml-lg-5 order-1 order-lg-2"
         />
       </div>
-      
     </React.Fragment>
   );
 };
