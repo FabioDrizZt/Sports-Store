458b93f1683169bdfa8b813ae0878a9e771b2acd
Jose <joc13th@gmail.com>
2020-09-22 12:55:00 -0300

    Routes comments

--- api/src/routes/order.js
+++ api/src/routes/order.js
@@ -2,65 +2,84 @@ const server = require("express").Router();
 const { Cart, Order } = require("../db");
 const check = require("./check.js");
 
-// S44 : Crear ruta que retorne todas las ordenes
-// Esta ruta puede recibir el query string status y deberá devolver sólo las ordenes con ese status. 
+/* S44: Crear ruta que retorne todas las ordenes
+Esta ruta puede recibir el query string status y deberá devolver sólo las ordenes con ese status.
+GET /orders*/
 server.get("/", check.isAuth, check.isAdmin, (req, res) => {
-    const status = req.query.status;
-    Cart.findAll({
-        where: { state: status },
-        include: [{ model: Order }]
-    }).then(carts => { res.send(carts) })
-        .catch(error => res.send(error))
-})
-//S46 : Crear Ruta que retorne una orden en particular. GET /orders/:id
-server.get('/:id', check.isAuth, (req, res) => {
-    Cart.findOne({
-        where: { id: req.params.id },
-        include: [{ model: Order }]
-    }).then((cart) => res.send(cart))
-        .catch((e) => res.status(400).json(e))
-})
+  const status = req.query.status;
+  Cart.findAll({
+    where: { state: status },
+    include: [{ model: Order }],
+  })
+    .then((carts) => {
+      res.send(carts);
+    })
+    .catch((error) => res.send(error));
+});
+
+/*S46 : Crear Ruta que retorne una orden en particular. 
+GET /orders/:id*/
+server.get("/:id", check.isAuth, (req, res) => {
+  Cart.findOne({
+    where: { id: req.params.id },
+    include: [{ model: Order }],
+  })
+    .then((cart) => res.send(cart))
+    .catch((e) => res.status(400).json(e));
+});
 
 // Traer una orden
 // GET /orders/:id
-server.get('/:id', check.isAuth, (req, res) => {
-    Order.findOne({
-        where: { id: req.params.id }
-    }).then((order) => res.send(order))
-        .catch((e) => res.status(400).json(e))
-})
+server.get("/:id", check.isAuth, (req, res) => {
+  Order.findOne({
+    where: { id: req.params.id },
+  })
+    .then((order) => res.send(order))
+    .catch((e) => res.status(400).json(e));
+});
 
-//S47 : Crear Ruta para modificar una Orden
-// PUT /orders/:id
+/*S47 : Crear Ruta para modificar una Orden
+ PUT /orders/:id*/
 server.put("/:id", check.isAuth, check.isAdmin, (req, res) => {
-    console.log(req.body.state)
-    Cart.findOne({ where: { id: req.params.id } })
-    .then(cart => {
-      cart.update({
-       state:req.body.state
-      }).then((c) => res.status(200).json(c))
+  console.log(req.body.state);
+  Cart.findOne({ where: { id: req.params.id } })
+    .then((cart) => {
+      cart
+        .update({
+          state: req.body.state,
+        })
+        .then((c) => res.status(200).json(c));
     })
-    .catch((error) => { res.status(400).json(error); })
-})
+    .catch((error) => {
+      res.status(400).json(error);
+    });
+});
+
 //SXX : Crear Ruta para Cerrar un Carrito
 // PATCH /orders/:id
 server.patch("/:id", check.isAuth, (req, res) => {
-    Cart.update({
-        state: "completa",
-    }, { where: { id: req.params.id } }
-    ).then((order) => res.status(200).send(order))
-        .catch((err) => { res.status(400).json({ err }); })
-})
+  Cart.update(
+    {
+      state: "completa",
+    },
+    { where: { id: req.params.id } }
+  )
+    .then((order) => res.status(200).send(order))
+    .catch((err) => {
+      res.status(400).json({ err });
+    });
+});
 
 // Eliminar una orden del carrito
 // DELETE /orders/:id
 server.delete("/:orderId", check.isAuth, (req, res) => {
-    Order.destroy({
-        where: { id: req.params.orderId }
-    }).then((deletedRecord) => {
-        if (deletedRecord === 1) res.status(200).json({ message: "Se eliminó su orden del carrito" });
-        else res.status(400).json({ message: "Orden no encontrada" });
-    });
+  Order.destroy({
+    where: { id: req.params.orderId },
+  }).then((deletedRecord) => {
+    if (deletedRecord === 1)
+      res.status(200).json({ message: "Se eliminó su orden del carrito" });
+    else res.status(400).json({ message: "Orden no encontrada" });
+  });
 });
 
 module.exports = server;
--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -6,24 +6,41 @@ const check = require("./check.js");
 server.get("/", (req, res, next) => {
   Product.findAll({
     include: [{ model: Category }],
-  }).then(products => { res.send(products) })
-    .catch(error => { res.status(400).json({ error }) })
-})
+  })
+    .then((products) => {
+      res.send(products);
+    })
+    .catch((error) => {
+      res.status(400).json({ error });
+    });
+});
+
 // S21 : Crear ruta que devuelva todas las categories
 server.get("/categories", (req, res, next) => {
   Category.findAll()
-    .then((categories) => { res.send(categories); })
-    .catch(error => { res.status(400).json({ error }) })
+    .then((categories) => {
+      res.send(categories);
+    })
+    .catch((error) => {
+      res.status(400).json({ error });
+    });
 });
+
 //S22 : Crear Ruta que devuelva los productos de X categoria    *******Falla*********
 // "GET /products/categoria/:nombreCat
 // Retorna todos los productos de {nombreCat} Categoría."
 server.get("/category/:nombreCat", (req, res) => {
   Product.findAll({
     include: [{ model: Category, where: { name: req.params.nombreCat } }],
-  }).then(products => { res.send(products) })
-    .catch(error => { res.status(400).json({ error }) })
+  })
+    .then((products) => {
+      res.send(products);
+    })
+    .catch((error) => {
+      res.status(400).json({ error });
+    });
 });
+
 //S23: Crear ruta que retorne productos segun el keyword de búsqueda
 // /search?query=valor
 server.get("/search", (req, res) => {
@@ -31,30 +48,37 @@ server.get("/search", (req, res) => {
     where: {
       [Op.or]: [
         { name: { [Op.like]: "%" + req.query.query.toLowerCase() + "%" } },
-        { description: { [Op.like]: "%" + req.query.query.toLowerCase() + "%" } },
+        {
+          description: { [Op.like]: "%" + req.query.query.toLowerCase() + "%" },
+        },
       ],
     },
-  }).then((products) => res.status(200).json(products))
+  })
+    .then((products) => res.status(200).json(products))
     .catch((error) => res.status(400).json({ error }));
 });
+
 //S24 : Crear ruta de producto individual, pasado un ID que retorne un producto con sus detalles
 //GET /products/:id
 //Retorna un objeto de tipo producto con todos sus datos. (Incluidas las categorías e imagenes).
 server.get("/:id", (req, res) => {
   Product.findOne({
     where: { id: req.params.id },
-  }).then((products) => res.status(200).json(products))
+  })
+    .then((products) => res.status(200).json(products))
     .catch((error) => res.status(400).json({ error }));
 });
+
 /*S57 : Crear Ruta para obtener todas las reviews de un producto.
 GET /product/:id/review/*/
 server.get("/:productId/review", (req, res) => {
   Review.findAll({
-    where: { productId: req.params.productId }
+    where: { productId: req.params.productId },
   })
     .then((reviews) => res.send(reviews))
     .catch((error) => res.status(400).json({ error }));
-})
+});
+
 // S17 : Crear ruta para agregar categorias de un producto.
 // POST /products/:idProducto/category/:idCategoria
 // Agrega la categoria al producto.
@@ -65,11 +89,14 @@ server.post("/", check.isAuth, check.isAdmin, (req, res) => {
     description: req.body.description,
     price: req.body.price,
     stock: req.body.stock,
-    image: req.body.image
-  }).then(product => {
-    res.status(200).send(product);
-  }).catch(err => res.send(err))
+    image: req.body.image,
+  })
+    .then((product) => {
+      res.status(200).send(product);
+    })
+    .catch((err) => res.send(err));
 });
+
 // S18 : Crear ruta para crear/agregar Categoria
 // POST /products/category/
 server.post("/category", check.isAuth, check.isAdmin, (req, res) => {
@@ -77,14 +104,25 @@ server.post("/category", check.isAuth, check.isAdmin, (req, res) => {
     .then((category) => res.send(category))
     .catch((err) => res.send(err));
 });
+
 // S17 : Crear ruta para agregar categorias de un producto.
 // POST /products/:idProducto/category/:idCategoria
 // Agrega la categoria al producto.
-server.post("/:idProducto/category/:idCategoria", check.isAuth, check.isAdmin, (req, res) => {
-  productcategory.create({ productId: req.params.idProducto, categoryId: req.params.idCategoria })
-    .then((pc) => res.send(pc))
-    .catch((err) => res.send(err));
-});
+server.post(
+  "/:idProducto/category/:idCategoria",
+  check.isAuth,
+  check.isAdmin,
+  (req, res) => {
+    productcategory
+      .create({
+        productId: req.params.idProducto,
+        categoryId: req.params.idCategoria,
+      })
+      .then((pc) => res.send(pc))
+      .catch((err) => res.send(err));
+  }
+);
+
 // S54 : Crear ruta para crear/agregar Review
 // POST /products/:id/review
 server.post("/:id/review", check.isAuth, (req, res) => {
@@ -97,33 +135,40 @@ server.post("/:id/review", check.isAuth, (req, res) => {
       userId: req.body.userId,
       productId: req.params.id,
       description: req.body.description,
-      score: req.body.score
-    }
-  }).then((review) => {
-    if (review[1]===false) review[0].update(
-      {
-        description: req.body.description,
-        score: req.body.score
-      }, 
-    ).then(rev => res.send(rev))
-    else (res.send(review))
-  }).catch((error) => res.send(error))
-})
+      score: req.body.score,
+    },
+  })
+    .then((review) => {
+      if (review[1] === false)
+        review[0]
+          .update({
+            description: req.body.description,
+            score: req.body.score,
+          })
+          .then((rev) => res.send(rev));
+      else res.send(review);
+    })
+    .catch((error) => res.send(error));
+});
 
 // S55 : Crear ruta para Modificar Review
 // PUT /product/:id/review/:idReview
-
 server.put("/:id/review/:idReview", check.isAuth, (req, res) => {
-  Review.findOne({ where: { id: req.params.idReview, productId: req.params.id } })
-    .then(review => {
-      review.update({
-        description: req.body.description,
-        score: req.body.score
-      }).then((r) => res.status(200).json(r))
+  Review.findOne({
+    where: { id: req.params.idReview, productId: req.params.id },
+  })
+    .then((review) => {
+      review
+        .update({
+          description: req.body.description,
+          score: req.body.score,
+        })
+        .then((r) => res.status(200).json(r));
     })
-    .catch((error) => { res.status(400).json(error); })
+    .catch((error) => {
+      res.status(400).json(error);
+    });
 });
-
 
 //S26 : Crear ruta para Modificar Producto
 // PUT /products/:id
@@ -131,69 +176,116 @@ server.put("/:id/review/:idReview", check.isAuth, (req, res) => {
 // Retorna 200 si se modificó con exito, y retorna los datos del producto modificado.
 server.put("/:id", check.isAuth, check.isAdmin, (req, res) => {
   Product.findOne({ where: { id: req.params.id } })
-    .then(product => {
-      product.update({
-        name: req.body.name,
-        size: req.body.size,
-        description: req.body.description,
-        price: req.body.price,
-        stock: req.body.stock,
-        image: req.body.image
-      }).then((p) => res.status(200).json(p))
+    .then((product) => {
+      product
+        .update({
+          name: req.body.name,
+          size: req.body.size,
+          description: req.body.description,
+          price: req.body.price,
+          stock: req.body.stock,
+          image: req.body.image,
+        })
+        .then((p) => res.status(200).json(p));
     })
-    .catch((error) => { res.status(400).json(error); })
+    .catch((error) => {
+      res.status(400).json(error);
+    });
 });
+
 //S20 : Crear ruta para Modificar Categoria
 // PUT /products/category/:id
 server.put("/category/:id", check.isAuth, check.isAdmin, (req, res, next) => {
-  Category.update({
-    name: req.body.name,
-    description: req.body.description
-  }, { where: { id: req.params.id } })
-    .then((category) => { res.status(200).json({ category }); })
-    .catch((error) => { res.status(400).json({ error }); });
+  Category.update(
+    {
+      name: req.body.name,
+      description: req.body.description,
+    },
+    { where: { id: req.params.id } }
+  )
+    .then((category) => {
+      res.status(200).json({ category });
+    })
+    .catch((error) => {
+      res.status(400).json({ error });
+    });
 });
+
 // S17 : Crear ruta para sacar categorias de un producto.
 // DELETE /products/:idProducto/category/:idCategoria
 // Elimina la categoria al producto.
-server.delete("/:idProducto/category/:idCategoria", check.isAuth, check.isAdmin, (req, res) => {
-  productcategory.destroy({
-    where: {
-      productId: req.params.idProducto,
-      categoryId: req.params.idCategoria,
-    },
-  }).then((deletedRecord) => {
-    if (deletedRecord === 1) res.status(200).json({ message: "Se elimino la categoria al producto" });
-    else res.status(400).json({ message: "Categoria no encontrada" });
-  }).catch((error) => { res.status(500).json(error); });
-});
+server.delete(
+  "/:idProducto/category/:idCategoria",
+  check.isAuth,
+  check.isAdmin,
+  (req, res) => {
+    productcategory
+      .destroy({
+        where: {
+          productId: req.params.idProducto,
+          categoryId: req.params.idCategoria,
+        },
+      })
+      .then((deletedRecord) => {
+        if (deletedRecord === 1)
+          res
+            .status(200)
+            .json({ message: "Se elimino la categoria al producto" });
+        else res.status(400).json({ message: "Categoria no encontrada" });
+      })
+      .catch((error) => {
+        res.status(500).json(error);
+      });
+  }
+);
+
 // S27 eliminar un producto DELETE /products/:id
 // Retorna 200 si se elimino con exito.
 server.delete("/:id", check.isAuth, check.isAdmin, (req, res) => {
   Product.destroy({ where: { id: req.params.id } })
-    .then(deletedRecord => {
-      if (deletedRecord === 1) res.status(200).json({ message: "Se elimino el producto" });
+    .then((deletedRecord) => {
+      if (deletedRecord === 1)
+        res.status(200).json({ message: "Se elimino el producto" });
       else res.status(400).json({ message: "Producto no encontrado" });
-    }).catch((error) => { res.status(500).json(error); });
-})
+    })
+    .catch((error) => {
+      res.status(500).json(error);
+    });
+});
+
 //S19 : Crear Ruta para eliminar Categoria
 // DELETE /products/category/:id
 server.delete("/category/:id", check.isAuth, check.isAdmin, (req, res) => {
   Category.destroy({
     where: { id: req.params.id },
-  }).then((deletedRecord) => {
-    if (deletedRecord === 1) res.status(200).json({ message: "Su categoria fue eliminado satisfactoriamente." });
-    else res.status(400).json({ message: "categoria no encontrado." });
-  }).catch((error) => { res.status(500).json(error); });
+  })
+    .then((deletedRecord) => {
+      if (deletedRecord === 1)
+        res
+          .status(200)
+          .json({ message: "Su categoria fue eliminado satisfactoriamente." });
+      else res.status(400).json({ message: "categoria no encontrado." });
+    })
+    .catch((error) => {
+      res.status(500).json(error);
+    });
 });
-
+/*S56: Crear ruta para eliminar Review
+DELETE /product/:id/review/:idReview */
 server.delete("/:id/review/:idReview", check.isAuth, (req, res) => {
   Review.destroy({
-    where: { id: req.params.idReview }
-  }).then((deletedRecord) => {
-    if (deletedRecord === 1) res.status(200).json({ message: "Su review fue eliminado satisfactoriamente." });
-    else res.status(400).json({ message: "review no encontrado." });
-  }).catch((error) => { res.status(500).send(error); });
+    where: { id: req.params.idReview },
+  })
+    .then((deletedRecord) => {
+      if (deletedRecord === 1)
+        res
+          .status(200)
+          .json({ message: "Su review fue eliminado satisfactoriamente." });
+      else res.status(400).json({ message: "review no encontrado." });
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
 });
 
 module.exports = server;
