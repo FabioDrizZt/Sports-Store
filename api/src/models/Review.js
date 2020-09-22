723c77f55710f7ff8cf083f410296d7996f521f2
Jose <joc13th@gmail.com>
2020-09-22 12:31:19 -0300

    s53 comment

--- api/src/models/Review.js
+++ api/src/models/Review.js
@@ -1,16 +1,19 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 
+//S53: Crear Modelo de Reviews
 module.exports = (sequelize) => {
-  sequelize.define('review', {
+  sequelize.define("review", {
     description: {
-        type: DataTypes.TEXT,
-        defaultValue: "Este producto no tiene descripción.",
-        set(value) { this.setDataValue('description', value.toLowerCase()) }
+      type: DataTypes.TEXT,
+      defaultValue: "Este producto no tiene descripción.",
+      set(value) {
+        this.setDataValue("description", value.toLowerCase());
+      },
     },
     score: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        validate: { min: 0 }
-      },
-  })
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      validate: { min: 0 },
+    },
+  });
 };
