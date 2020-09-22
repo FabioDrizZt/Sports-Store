5938c4565da3bd06ec90eb0f7c9d99a59c92fc51
arieltecay <arieltecay@gmail.com>
2020-09-22 20:06:26 -0300

    update2

--- client/src/Components/Cart.jsx
+++ client/src/Components/Cart.jsx
@@ -12,7 +12,7 @@ const Cart = (carrito) => {
   const cart = useSelector((state) => state.cart);
 
 
-  const getProducts = window.localStorage.getItem('myCart')
+  const getProducts = JSON.parse(localStorage.getItem("myCart"));
   const [productCards, setProductsCards] = useState([])
 
   const [changeCart, setChangeCart] = useState("");
@@ -20,17 +20,14 @@ const Cart = (carrito) => {
   const [total, setTotal] = useState(0);
 
   const getGuestProductsOfCart = () => {
-    var prods
     if (getProducts) {
-      prods = JSON.parse(getProducts)
-      setProductsCards(prods)
+      setProductsCards(getProducts)
     } else setProductsCards([])
   }
 
   useEffect(() => {
     // si no esta logeado se rompe porque el user.id de alguien no logeado con items es undefined
     if (user.id) {
-
       dispatch(getCartUser(user.id))
     }
     else {
@@ -40,70 +37,127 @@ const Cart = (carrito) => {
     }
   }, [changeCart]);
 
-  console.log("Carrito: " + cart);
-  console.log("Carrito Guest: "+ productCards);
+  console.log("Carrito: " + cart.map((e)=> e.id));
+  console.log("Carrito: " + cart.map((e)=> e.product.id));
 
+  console.log("Carrito Guest: " + productCards);
 
-  return (
-    <div className={s.container}>
-      {cart && cart.map((ord) => <Order order={ord} />)}
-      <div className='contenedor-del-contenedor'>
-        {/* {productCards.map((ord) => <Order order={ord} /> )} */}
-        
-      </div>
-      <div className={s.subtotal}>
-        <h2>TOTAL DE LA COMPRA</h2>
-        <h3 className={s.price}>${total}</h3>
-      </div>
-      {total > 0 ? (
-        <div className={s.flex}>
-          <Link to={`/cart/checkout`}>
-            <button className="btn btn-outline-success">REALIZAR COMPRA</button>
-          </Link>
+  if (user.id) {
+    return (
+      <div className={s.container}>
+        { cart && cart.map((ord) => <Order order={ord} />)}
+        <div className={s.subtotal}>
+          <h2>TOTAL DE LA COMPRA</h2>
+          <h3 className={s.price}>${total}</h3>
         </div>
-      ) : null}
-      <div>
-        <Route
-          exact
-          path="/cart/checkout"
-          render={() => <Checkout total={total} />}
-        />
-      </div>
-      <button
-        className="btn btn-success mt-4"
-        onClick={() => window.history.back()}
-      >
-        Volver
-      </button>
-      {cart && cart.length === 0 ? (
-        <div className="mt-4">
-          <h1>Tu carrito está vacio!</h1>
-          <h2>
-            <Link to="/products">Ir al cátalogo</Link>
-          </h2>
+        {total > 0 ? (
+          <div className={s.flex}>
+            <Link to={`/cart/checkout`}>
+              <button className="btn btn-outline-success">REALIZAR COMPRA</button>
+            </Link>
+          </div>
+        ) : null}
+        <div>
+          <Route
+            exact
+            path="/cart/checkout"
+            render={() => <Checkout total={total} />}
+          />
         </div>
-      ) : (
-          <button
-            className="btn btn-danger mt-4"
-            onClick={() => dispatch(removeCart(user.id))}
-          >
-            Vaciar Carrito
-          </button>
-        )}
-      <button
-        className="btn btn-success mt-4"
-        onClick={() =>
-          setTotal(
-            cart.reduce(function (prev, cur) {
-              return prev + cur.product.price * cur.amount;
-            }, 0)
-          )
-        }
-      >
-        Calcular Total
+        <button
+          className="btn btn-success mt-4"
+          onClick={() => window.history.back()}
+        >
+          Volver
       </button>
-    </div>
-  );
+        {cart && cart.length === 0 ? (
+          <div className="mt-4">
+            <h1>Tu carrito está vacio!</h1>
+            <h2>
+              <Link to="/products">Ir al cátalogo</Link>
+            </h2>
+          </div>
+        ) : (
+            <button
+              className="btn btn-danger mt-4"
+              onClick={() => dispatch(removeCart(user.id))}
+            >
+              Vaciar Carrito
+            </button>
+          )}
+        <button
+          className="btn btn-success mt-4"
+          onClick={() =>
+            setTotal(
+              cart.reduce(function (prev, cur) {
+                return prev + cur.product.price * cur.amount;
+              }, 0)
+            )
+          }
+        >
+          Calcular Total
+      </button>
+      </div>
+    );
+  } else {
+    return (
+      <div className={s.container}>
+        {productCards && productCards.map ((e) => <Order order = {e} />)} 
+        <div className={s.subtotal}>
+          <h2>TOTAL DE LA COMPRA</h2>
+          <h3 className={s.price}>${total}</h3>
+        </div>
+        {total > 0 ? (
+          <div className={s.flex}>
+            <Link to={`/cart/checkout`}>
+              <button className="btn btn-outline-success">REALIZAR COMPRA</button>
+            </Link>
+          </div>
+        ) : null}
+        <div>
+          <Route
+            exact
+            path="/cart/checkout"
+            render={() => <Checkout total={total} />}
+          />
+        </div>
+        <button
+          className="btn btn-success mt-4"
+          onClick={() => window.history.back()}
+        >
+          Volver
+        </button>
+        {cart && cart.length === 0 ? (
+          <div className="mt-4">
+            <h1>Tu carrito está vacio!</h1>
+            <h2>
+              <Link to="/products">Ir al cátalogo</Link>
+            </h2>
+          </div>
+        ) : (
+            <button
+              className="btn btn-danger mt-4"
+              onClick={() => dispatch(removeCart(user.id))}
+            >
+              Vaciar Carrito
+            </button>
+          )}
+        <button
+          className="btn btn-success mt-4"
+          onClick={() =>
+            setTotal(
+              cart.reduce(function (prev, cur) {
+                return prev + cur.product.price * cur.amount;
+              }, 0)
+            )
+          }
+        >
+          Calcular Total
+        </button>
+      </div>
+    );
+
+  }
 };
 
 export default Cart;
