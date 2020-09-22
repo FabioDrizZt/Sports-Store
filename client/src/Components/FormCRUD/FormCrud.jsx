8a72e20d41e3eafce3ff274fca9fbd754f307f4d
arieltecay <arieltecay@gmail.com>
2020-09-22 16:14:45 -0300

    Agregue el UseState

--- client/src/Components/FormCRUD/FormCrud.jsx
+++ client/src/Components/FormCRUD/FormCrud.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import NavBarAdmin from "../NavBar/NavBarAdmin";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
