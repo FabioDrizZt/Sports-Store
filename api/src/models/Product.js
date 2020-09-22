37c8fe39074f60c97387192c91b897e48f9bc471
Jose <joc13th@gmail.com>
2020-09-22 12:29:29 -0300

    s4 comment

--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -1,30 +1,39 @@
-const { DataTypes } = require('sequelize');
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
+const { DataTypes } = require("sequelize");
+
+//S4: Crear modelo de productos
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('product', {
+  sequelize.define("product", {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
-      set(value) { this.setDataValue('name', value.toLowerCase()) }
-    }, size: {
+      set(value) {
+        this.setDataValue("name", value.toLowerCase());
+      },
+    },
+    size: {
       type: DataTypes.STRING,
-    }, description: {
+    },
+    description: {
       type: DataTypes.TEXT,
       defaultValue: "Este producto no tiene descripción.",
-      set(value) { this.setDataValue('description', value.toLowerCase()) }
-    }, price: {
+      set(value) {
+        this.setDataValue("description", value.toLowerCase());
+      },
+    },
+    price: {
       type: DataTypes.FLOAT,
       allowNull: false,
-      validate: { min: 0 }
-    }, stock: {
+      validate: { min: 0 },
+    },
+    stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      validate: { min: 0 }
-    }, image: {
+      validate: { min: 0 },
+    },
+    image: {
       type: DataTypes.STRING,
-      defaultValue: "https://bagwellpromotions.com/wp-content/uploads/2014/03/sports.jpg",
-    }
+      defaultValue:
+        "https://bagwellpromotions.com/wp-content/uploads/2014/03/sports.jpg",
+    },
   });
 };
