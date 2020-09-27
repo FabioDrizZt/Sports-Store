759ad818113ba25e9c677918df27ff32bd85dc74
Gabriel Apaza <gabriel.oaq1@gmail.com>
2020-09-27 17:29:00 -0300

    carousel en home

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -2,6 +2,8 @@ import React, { useEffect }from "react";
 import { addtoCart, getOrders } from "../redux/actions";
 import { useSelector, useDispatch } from "react-redux";
 import {Link} from "react-router-dom";
+import { MDBCarousel, MDBCarouselCaption, MDBCarouselInner, MDBCarouselItem, MDBView, MDBMask, MDBContainer } from
+"mdbreact";
 import "./Home.css";
 
 function Home(){
@@ -34,11 +36,37 @@ function Home(){
         }
     }, []);
 
-   return( 
+return( 
     <Link to="/products">	
-    <div className="home"> 
-
-    </div>
+        <div>
+        <div>
+        <MDBContainer>
+        <MDBCarousel activeItem={1} length={3} showControls={true} showIndicators={true} className="z-depth-1">
+        <MDBCarouselInner>
+            <MDBCarouselItem itemId="1">
+            <MDBView>
+                <img className="d-block w-100 " src="https://www.decathlon.cl/modules/ps_imageslider/images/58da9df6d954063b69c67b4b047af5e00c98a652_respira-deporte-1.jpg" alt="First slide"/>
+            <MDBMask overlay="black-light" />
+            </MDBView>
+            </MDBCarouselItem>
+            <MDBCarouselItem itemId="2">
+            <MDBView>
+                <img className="d-block w-100" src="https://www.decathlon.cl/modules/ps_imageslider/images/ab700f11761ed107e5088c43a3b7c9d99ded6ede_ACTIVADEPORTE-DECATHLON-DESKTOP.jpg" alt="Second slide"/>
+            <MDBMask overlay="black-strong" />
+            </MDBView>
+            </MDBCarouselItem>
+            <MDBCarouselItem itemId="3">
+            <MDBView>
+                <img className="d-block w-100" src="https://www.decathlon.cl/modules/ps_imageslider/images/eded4df0afd840bb14c265c85881842a62ba3b6e_compra-segura.jpg" alt="Third slide"/>
+            <MDBMask overlay="black-slight" />
+            </MDBView>
+            </MDBCarouselItem>
+            </MDBCarouselInner>
+            </MDBCarousel>
+            </MDBContainer>
+        </div>
+        <div>
+        </div></div>    
     </Link>	
     )
 }
